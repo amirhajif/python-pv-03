6a7e01bac45802f68e52ebf1eef7870d2b64bcb9
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-02-01 21:24:16 +0330

    Create session-1.py

---
+++ session-1.py
@@ -0,0 +1,34 @@
+# print("hello world")
+
+# Variables
+# score=20
+# firstName="John"
+# lastName="cena"
+
+# print(score)
+
+# lowerCamelCase
+# firstName="John"
+# myOldPassword=12335
+
+# username=input("Enter your username: ")
+# print(username)
+
+# score=int(input("please enter score: "))
+# print(score)
+# print(type(score))
+
+# print("amirhossein",end=" ")
+# print("hajitabr")
+# print("programming Teacher")
+
+name="hooman zakaryaei"
+
+print(type(name))
+print(len(name))
+
+print(name[6])
+print(name[0:6])
+print(name[:6])
+print(name[7:])
+
