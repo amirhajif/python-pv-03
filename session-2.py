34c12e74ad7ca438575f3b5f8c5f226c598ac378
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-02-06 20:17:33 +0330

    Create session-2.py

---
+++ session-2.py
@@ -0,0 +1,76 @@
+''' Title '''
+# address="babol keshvari sardaran"
+
+# print(address.title())
+
+# titleCased_address=address.title()
+# print(titleCased_address)
+
+''' Upper '''
+# name="amirhossein"
+# print(name.upper())
+
+''' lower '''
+# name=input()
+# print(name.lower())
+
+''' Replace'''
+# phone="my new iphone"
+# print(phone.replace('n','z'))
+
+''' Split '''
+# phones='iphone,samsung,nokia,sony'
+# print(phones.split(','))
+
+''' lStrip,Rstrip,Strip '''
+# username="  amir"
+# print(username.lstrip())
+# print(username.rstrip())
+# print(username.strip())
+
+''' Fstring '''
+# firstName="amir"
+# lastName="hajitabar"
+# message=f"my name is {firstName} and lastname is {lastName}"
+# print(message)
+
+''' Format '''
+# firstName="amir"
+# lastName="hajitabar"
+# message="my name is {} and lastname is {}".format(firstName,lastName)
+# print(message)
+
+''' \n \t'''
+# print("amirhossein\nHajitabar")
+# print("amirhossein\tHajitabar")
+
+''' Excersice '''
+# pl1=input("Enter your favorite programming language: ")
+# pl2=input("Enter your favorite programming language: ")
+# pl3=input("Enter your favorite programming language: ")
+# name=input("Enter your name: ")
+
+# print(f"my name is {name.title()}:\n\t1-{pl1.upper()}\n\t2-{pl2.upper()}\n\t3-{pl3.upper()}")
+
+''' Numbers '''
+# a=5
+# b=2
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+# print(a**b)
+
+# 1,000,000,000
+# money=1_000_000_000
+# print(money)    
+
+''' Multiple Assignment '''
+# username="amir"
+# password="amir123"
+# city="babol"
+# username,password,city="amir","amir123","babol"
+
