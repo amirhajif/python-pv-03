a44bc32a7e766b29106359cacefb65d914df872b
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-02-22 21:40:25 +0330

    Create session-4.py

---
+++ session-4.py
@@ -0,0 +1,115 @@
+# cars=["bmw","pride","audi"]
+# # cars.sort()
+# print(sorted(cars))
+# print(cars)
+
+# a=[1,2,3]
+# b=a
+# c=[1,2,3]
+
+# print(a is b)
+# print(a is c)
+
+
+# slicing
+# users=["amir","reza","homan","mamad"]
+# print(users[:])
+# print(users[1:4])
+# print(users[-1])
+# print(users[1:])
+# print(users[:4])
+# print(users[-1:])
+# print(users[:-1])
+
+# programmingStudent=["amir","reza","ali"]
+# allStudents=programmingStudent[:]
+
+# allStudents.append("hooman")
+# programmingStudent.append("jafar")
+
+# print(allStudents) #["amir","reza","ali","homan","jafar"]
+# print(programmingStudent) #["amir","reza","ali","homan","jafar"]
+
+# score=int(input("please enter score: "))
+# if score>=10:
+#     print("pass")
+# else:
+#     print("fail")
+
+
+# admin
+# username=input("please enter username: ")
+# if username=="admin":
+#     print("you are super user")
+# else:
+#     print("you are a simple user")
+
+
+# check 2 numbers value
+# a=int(input("please enter a: "))
+# b=int(input("please enter b: "))
+# if a==b:
+#     print("equal")
+# else:
+#     print("not-equal")
+
+# a=int(input("please enter a: "))
+# b=int(input("please enter b: ")) 
+# if a!=b:
+#     print("not-equal")
+# else:
+#     print("equal")
+
+# username=input("please enter username: ")
+# password=input("please enter password: ")
+
+# # admin admin
+# if username=="admin" and password=="admin":
+#     print("super user")
+# else:
+#     print("not-super user")
+
+# ch=input("please enter letter: ")
+# if ch=="a" or ch=="e" or ch=="o" or ch=="u" or ch=="i":
+#     print("vowel Sound")
+# else:
+#     print("not vowel-sound")
+
+# a--> mosbat,manfi,zero
+# a=int(input("please enter number: "))
+# if a>0:
+#     print("mosbat")
+# elif a==0:
+#     print("zero")
+# else:
+#     print("manfi")
+
+
+
+# users=[input(),input(),input(),input()]
+# new_name=input()
+# users.append(new_name)
+
+# users=["amir","reza","jafar","hooman"]
+# username=input("please enter username: ")
+
+# if username in users:
+#     print("login ok")
+# else:
+#     print("need signup")
+
+
+# users=["amir","reza","jafar","hooman"]
+# username=input("please enter username: ")
+
+# if username not in users:
+#     print("need signup")
+# else:
+#     print("signin ok")
+
+# users=["hooman"]
+
+# if users:
+#     print("not-empty")
+# else:
+#     print("empty")
