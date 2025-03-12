3f1581601a2d99c63aeaeccf2f72262766f21325
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-03-12 21:31:11 +0330

    Create session-8.py

---
+++ session-8.py
@@ -0,0 +1,70 @@
+# Way 1
+# n=int(input())
+# print("W",end="")
+# for i in range(0,n):
+#     print("o",end="")
+# print("w!")
+
+# Way 2
+# n = int(input())
+# print("W" + "o" * n + "w!")
+
+# Break
+# for i in range(10):
+#     if i==6:
+#         break
+#     print(i)
+# print("End")
+
+# Continue
+# for i in range(10):
+#     if i==6:
+#         continue
+#     print(i)
+# print("End")
+# 0 1 2 3 4 5 7 8 9 End
+
+
+# While Loop
+# for i in range(0,10):
+#     print("hello")
+
+# i=0
+# while i<10:
+#     print("hello")
+#     i+=1
+
+# factorial
+# n=int(input())
+# i=1
+# factorial=1
+# while i<=n:
+#     factorial*=i
+#     # i+=1
+#     i=i+1
+# print(factorial)
+
+# While-True - حلقه بی نهایت
+# daryaf adad az karbar va namayesh ta zamani ke -1 vared nakard
+# while True:
+#     n=int(input())
+#     if n==-1:
+#         break
+#     print(n)
+
+'''
+# دریافت نمرات دانش آموزان و
+#  نمایش معدل کلاس
+#  تا زمانی که معلم عدد -۲۰ وارد نکرد دریافت نمره ادامه داشته باشد
+'''
+
+# jamNomre,tedadNomre=0,0
+# while True:
+#     score=int(input("Please enter the score of the student: "))
+#     if score==-20:
+#         break
+#     jamNomre+=score
+#     tedadNomre+=1
+
+# print(jamNomre/tedadNomre)
+    
