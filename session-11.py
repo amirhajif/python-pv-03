6b752ea205b69611a4ba01f37cdb6bb5ee7c7971
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-03-31 16:13:22 +0330

    Create session-11.py

---
+++ session-11.py
@@ -0,0 +1,55 @@
+'''
+collections:
+1-list++
+2-tuple+
+3-dictionary++
+4-set-
+'''
+
+# cars={"bmw","pride","audi","benz","pride"}
+# print(type(cars))
+# print(cars)
+# duplicate dont allowed
+
+# loop on set Items
+# for car in cars:
+#     print(car)
+
+# add item
+# cars.add("bugatti")
+# print(cars)
+
+# remove
+# cars.remove("bmw")
+# print(cars)
+
+
+# discard
+# cars.discard("bmw")
+# print(cars)
+
+# diffrence of discard and remove - remove get error but discard dont error
+# cars.remove("mercedes")
+# print(cars)
+
+# cars.discard("mercedes")
+# print(cars)
+
+# autoCars={"bmw","benz","pride"}
+# homeCars={"zhian","peykan","xantia"}
+
+# myCars=autoCars.union(homeCars)
+# print(myCars)
+
+# autoCars.update(homeCars)
+# print(autoCars)
+
+
+autoCars={"bmw","benz","pride","lexus"}
+homeCars={"zhian","lexus","peykan","pride","xantia"}
+
+# intersection=autoCars.intersection(homeCars)
+# print(intersection)
+
+# autoCars.intersection_update(homeCars)
+# print(autoCars)
