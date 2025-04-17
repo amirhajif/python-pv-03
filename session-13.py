6b308485e382f12966b2f12927c7027000b51f85
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-04-17 18:57:56 +0330

    Create session-13.py

---
+++ session-13.py
@@ -0,0 +1,68 @@
+# Function
+# definition
+# def sayHello():
+#     print("hello")
+
+# # call
+# sayHello()
+
+# number=int(input("please enter number: "))
+# print(number*2)
+
+# def multiplyTwo():
+#     number=int(input("please enter number: "))
+#     print(number*2)
+
+
+# multiplyTwo()
+
+# numbers=[1,2,3,4,5]
+# print(sum(numbers))
+
+
+# def multiplyTwo(number):
+#     print(number*2)
+
+# x=int(input("please enter number: "))
+# multiplyTwo(x)
+
+# function for get number and show odd or even
+# def OddOrEven(number):
+#     if number%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+
+# adad=int(input("please enter number: "))
+# OddOrEven(adad)
+
+# can pass more than one argumants
+# get width and height of rectangle and calculate its area
+# def area(tol,arz):
+#     print(tol*arz)
+
+# tol,arz=int(input("please enter tol: ")),int(input("please enter arz: "))
+# area(tol,arz)
+
+# function for calculate factoriel
+# def factoriel(number):
+#     fact=1
+#     for i in range(1,number+1):
+#         fact*=i
+#     print(fact)
+
+# number=int(input("please enter number: "))
+# factoriel(number)
+
+
+# function Keyword
+# def introduce(name,city,age):
+#     print(f"my name is {name} and from {city} and {age} year`s old")
+
+# introduce(age=23,name="amir",city="babol")
+
+# default value
+# def introduce(name,city,age=23):
+#     print(f"my name is {name} and from {city} and {age} year`s old")
+
+# introduce("amir","babol")
